95415bdd56ed9a69373c1b11b2459bf90efa6153
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-02-24 12:41:15 -0300

    footer

--- app/views/structures/footer.php
+++ app/views/structures/footer.php
@@ -1,1 +1,28 @@
-<h1>footer</h1>
+<div class="container-fluid footer ">
+    <div class="row p-5 bg-dark text-white">
+        <div class="col-xs-12 col-md-6 text-center">
+            <img src="<?php echo $PRINCIPAL ?>/assets/logo.png" alt="inicio" width="70" class="d-inline-block align-text-top">
+            <p class="footer-subtitle text-secondary mt-1">Universidad Nacional del Comahue</p>
+            <p class="footer-subtitle text-secondary">Programación Web Dinámica</p>
+            <p class="footer-subtitle text-secondary">2024</p>
+        </div>
+        <div class="col-xs-12 col-md-6">
+            <h5 class="mb-3 footer-title">Contacto</h5>
+            <div class="mb-2">
+                <img src="<?php echo $PRINCIPAL ?>/views/icons/icons/github.svg" alt="">
+                <a class="text-white footer-subtitle text-decoration-none" href="https://github.com/NachoCayuqueo" target="_blank">GitHub</a>
+            </div>
+            <div class="mb-2">
+                <img src="<?php echo $PRINCIPAL ?>/views/icons/icons/envelope-fill.svg" alt="">
+                <a class="text-white footer-subtitle text-decoration-none" href="#">nacho.cayuqueo@est.fi.uncoma.edu.ar</a>
+            </div>
+            <div class="mb-2">
+                <img src="<?php echo $PRINCIPAL ?>/views/icons/icons/envelope-fill.svg" alt="">
+                <a class="text-white footer-subtitle text-decoration-none" href="#">cayuqueonacho@gmail.com</a>
+            </div>
+        </div>
+        <div class="col-xs-12">
+            <p class="text-white footer-title text-center">&copy; Desarrollado por Cayuqueo, Nacho Emanuel</p>
+        </div>
+    </div>
+</div>
