f1e71e6a6de2a06aed25bac15f636ca6fa4d078a
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-02-28 11:44:40 -0300

    nueva carpeta para login y register

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <?php
-    $title = "Registro";
+    $title = "Login";
     include_once "../structures/head.php";
     ?>
 </head>
@@ -11,6 +11,11 @@
 <body>
     <div class="row vh-100 g-0">
         <!-- left side -->
+        <!-- d-none d-lg-block => tamaño chico, se esconde la imagen -->
+        <div class="col-lg-6 position-relative d-none d-lg-block">
+            <div class="bg-holder login-image"></div>
+        </div>
+        <!-- right side -->
         <div class="col-lg-6 ">
             <div class="row align-items-center justify-content-center h-100 g-0 px-4 px-sm-0">
                 <div class="col col-sm-6 col-lg-7 col-xl-6">
@@ -20,8 +25,8 @@
                     </a>
 
                     <div class="text-center mb-5">
-                        <h3 class="fw-bold">Registrarse</h3>
-                        <p class="text-secondary">Complete todos los campos</p>
+                        <h3 class="fw-bold">Iniciar Sesión</h3>
+                        <p class="text-secondary">obtener acceso a su cuenta</p>
                     </div>
 
                     <!-- form -->
@@ -36,36 +41,18 @@
                             <input type="password" class="form-control" id="password" name="password" placeholder="Ingrese contraseña" required minlength="8">
                             <!-- <div class="invalid-feedback">La contraseña debe tener como minimo 8 caracteres</div> -->
                         </div>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text"><img src="<?php echo $BOOTSTRAP_ICONS ?>/lock-fill.svg" alt="lock"></span>
-                            <input type="password" class="form-control" id="repeat-password" name="repeat-password" placeholder="Repetir contraseña" required minlength="8">
-                            <!-- <div class="invalid-feedback">La contraseña debe tener como minimo 8 caracteres</div> -->
-                        </div>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text"><img src="<?php echo $BOOTSTRAP_ICONS ?>/envelope-fill.svg" alt="lock"></span>
-                            <input type="text" class="form-control" id="email" name="email" placeholder="Ingrese Email" required>
-                            <!-- <div class="invalid-feedback">La contraseña debe tener como minimo 8 caracteres</div> -->
-                        </div>
                         <div class="mb-3">
-                            <input name=send id=send type=submit value="Register" class="btn btn-primary btn-lg w-100">
+                            <input name=send id=send type=submit value="Login" class="btn btn-primary btn-lg w-100">
                         </div>
 
                         <div class="text-center">
-                            <small>¿Quieres iniciar sesión?</small>
-                            <a href="login.php" class="fw-bold">Log In</a>
+                            <small>¿No tienes una cuenta?</small>
+                            <a href="../register" class="fw-bold">Registrate</a>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-        <!-- right side -->
-        <!-- d-none d-lg-block => tamaño chico, se esconde la imagen -->
-        <div class="col-lg-6 position-relative d-none d-lg-block">
-            <div class="bg-holder register-image"></div>
-        </div>
-
-
-
     </div>
 </body>
 
--- app/views/register/index.php
+++ app/views/register/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <?php
-    $title = "Login";
+    $title = "Registro";
     include_once "../structures/head.php";
     ?>
 </head>
@@ -11,11 +11,6 @@
 <body>
     <div class="row vh-100 g-0">
         <!-- left side -->
-        <!-- d-none d-lg-block => tamaño chico, se esconde la imagen -->
-        <div class="col-lg-6 position-relative d-none d-lg-block">
-            <div class="bg-holder login-image"></div>
-        </div>
-        <!-- right side -->
         <div class="col-lg-6 ">
             <div class="row align-items-center justify-content-center h-100 g-0 px-4 px-sm-0">
                 <div class="col col-sm-6 col-lg-7 col-xl-6">
@@ -25,8 +20,8 @@
                     </a>
 
                     <div class="text-center mb-5">
-                        <h3 class="fw-bold">Iniciar Sesión</h3>
-                        <p class="text-secondary">obtener acceso a su cuenta</p>
+                        <h3 class="fw-bold">Registrarse</h3>
+                        <p class="text-secondary">Complete todos los campos</p>
                     </div>
 
                     <!-- form -->
@@ -41,18 +36,36 @@
                             <input type="password" class="form-control" id="password" name="password" placeholder="Ingrese contraseña" required minlength="8">
                             <!-- <div class="invalid-feedback">La contraseña debe tener como minimo 8 caracteres</div> -->
                         </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><img src="<?php echo $BOOTSTRAP_ICONS ?>/lock-fill.svg" alt="lock"></span>
+                            <input type="password" class="form-control" id="repeat-password" name="repeat-password" placeholder="Repetir contraseña" required minlength="8">
+                            <!-- <div class="invalid-feedback">La contraseña debe tener como minimo 8 caracteres</div> -->
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><img src="<?php echo $BOOTSTRAP_ICONS ?>/envelope-fill.svg" alt="lock"></span>
+                            <input type="text" class="form-control" id="email" name="email" placeholder="Ingrese Email" required>
+                            <!-- <div class="invalid-feedback">La contraseña debe tener como minimo 8 caracteres</div> -->
+                        </div>
                         <div class="mb-3">
-                            <input name=send id=send type=submit value="Login" class="btn btn-primary btn-lg w-100">
+                            <input name=send id=send type=submit value="Register" class="btn btn-primary btn-lg w-100">
                         </div>
 
                         <div class="text-center">
-                            <small>¿No tienes una cuenta?</small>
-                            <a href="register.php" class="fw-bold">Registrate</a>
+                            <small>¿Quieres iniciar sesión?</small>
+                            <a href="../login" class="fw-bold">Log In</a>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
+        <!-- right side -->
+        <!-- d-none d-lg-block => tamaño chico, se esconde la imagen -->
+        <div class="col-lg-6 position-relative d-none d-lg-block">
+            <div class="bg-holder register-image"></div>
+        </div>
+
+
+
     </div>
 </body>
 
