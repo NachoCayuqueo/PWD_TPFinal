b31ac53442578a19ab33a5fa84f7d4ae46fc773a
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-02-28 17:24:56 -0300

    footer

--- app/views/structures/footer.php
+++ app/views/structures/footer.php
@@ -1,7 +1,7 @@
-<div class="container-fluid footer ">
-    <div class="row p-5 bg-dark text-white">
+<div class="container-fluid footer">
+    <div class="row p-5" style="background-color: #aabebd;">
         <div class="col-xs-12 col-md-6 text-center">
-            <img src="<?php echo $PRINCIPAL ?>/assets/logo.png" alt="inicio" width="70" class="d-inline-block align-text-top">
+            <img src="<?php echo $LOGOS ?>/logo.png" alt="inicio" width="70" class="d-inline-block align-text-top">
             <p class="footer-subtitle text-secondary mt-1">Universidad Nacional del Comahue</p>
             <p class="footer-subtitle text-secondary">Programación Web Dinámica</p>
             <p class="footer-subtitle text-secondary">2024</p>
@@ -10,19 +10,19 @@
             <h5 class="mb-3 footer-title">Contacto</h5>
             <div class="mb-2">
                 <img src="<?php echo $PRINCIPAL ?>/views/icons/icons/github.svg" alt="">
-                <a class="text-white footer-subtitle text-decoration-none" href="https://github.com/NachoCayuqueo" target="_blank">GitHub</a>
+                <a class=" footer-subtitle text-decoration-none" href="https://github.com/NachoCayuqueo" target="_blank">GitHub</a>
             </div>
             <div class="mb-2">
                 <img src="<?php echo $PRINCIPAL ?>/views/icons/icons/envelope-fill.svg" alt="">
-                <a class="text-white footer-subtitle text-decoration-none" href="#">nacho.cayuqueo@est.fi.uncoma.edu.ar</a>
+                <a class=" footer-subtitle text-decoration-none" href="#">nacho.cayuqueo@est.fi.uncoma.edu.ar</a>
             </div>
             <div class="mb-2">
                 <img src="<?php echo $PRINCIPAL ?>/views/icons/icons/envelope-fill.svg" alt="">
-                <a class="text-white footer-subtitle text-decoration-none" href="#">cayuqueonacho@gmail.com</a>
+                <a class=" footer-subtitle text-decoration-none" href="#">cayuqueonacho@gmail.com</a>
             </div>
         </div>
         <div class="col-xs-12">
-            <p class="text-white footer-title text-center">&copy; Desarrollado por Cayuqueo, Nacho Emanuel</p>
+            <p class=" footer-title text-center">&copy; Desarrollado por Cayuqueo, Nacho Emanuel</p>
         </div>
     </div>
 </div>
