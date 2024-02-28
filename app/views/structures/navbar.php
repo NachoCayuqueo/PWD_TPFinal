04e2f7ebc8bb58e0bab096de25163619dbbf39aa
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-02-28 11:42:12 -0300

    se actualizo navbar

--- app/views/structures/navbar.php
+++ app/views/structures/navbar.php
@@ -1,1 +1,48 @@
-<h1>navbar</h1>
+<nav class="navbar navbar-expand-lg" style="background-color: #aabebd;">
+    <div class="container-fluid">
+        <a class="navbar-brand m-2" href="<?php echo $PRINCIPAL ?>" type="button" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Home">
+            <img src="<?php echo $LOGOS ?>/logo_pet_shop.png" alt="inicio" width="80" class="d-inline-block align-text-top">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-title collapse navbar-collapse" id="navbarNav">
+
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-2">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="<?php echo $BOOTSTRAP_ICONS ?>/person-circle.svg" alt="inicio" width="30" class="d-inline-block align-text-top">
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><a class="dropdown-item" href="#">Login</a></li>
+                        <li><a class="dropdown-item" href="#">Register</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Configuraciones</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<hr class="my-0" style="color: white;">
+
+<nav class="navbar navbar-expand-lg" style="background-color: #aabebd;">
+    <div class="container-fluid">
+        <div class="navbar-title collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link" style="color: #f5f7f8;">PAGINA 1</a>
+                </li>
+                <li class="nav-item ms-5">
+                    <a href="#" class="nav-link" style="color: #f5f7f8;">PAGINA 2</a>
+                </li>
+                <li class="nav-item ms-5">
+                    <a href="#" class="nav-link" style="color: #f5f7f8;">PAGINA 3</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
