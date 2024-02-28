71671f9430d883be91bab1127c4fcd034d486323
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-02-28 11:46:23 -0300

    nuevas rutas globales

--- config/configuration.php
+++ config/configuration.php
@@ -11,13 +11,21 @@ $PROYECT = 'PWD_TPFinal';
 //* store the project directory
 $ROOT = $_SERVER['DOCUMENT_ROOT'] . "/$PROYECT/";
 
-
+//* direction to principal
 $PRINCIPAL = "http://" . $_SERVER['HTTP_HOST'] . "/$PROYECT";
+
+//* direction to router
+$ROUTER = $PRINCIPAL . "/router";
+
 //* direction to Bootstrap icons
 $BOOTSTRAP_ICONS = $PRINCIPAL . "/public/lib/bootstrap/bootstrap-icons/icons";
 
 //* direction to Logos
 $LOGOS = $PRINCIPAL . "/assets/logos";
+
+//* direction to Images
+$IMAGES = $PRINCIPAL . "/assets/images";
+
 //* .env configuration with phpdotenv
 require $ROOT . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable($GLOBALS['PRINCIPAL']);
