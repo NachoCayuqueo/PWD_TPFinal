35f2d6c4a52d9375119b8e15ff8d389804f9a2bd
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-02-29 11:27:16 -0300

    se agrego arreglo con mas info

--- app/views/structures/cards.php
+++ app/views/structures/cards.php
@@ -3,15 +3,16 @@
 /**
  * Genera una tarjeta HTML de producto con la información proporcionada.
  *
+ * @param string $cardId        El ID único de la tarjeta de producto.
  * @param string $urlImagen     La URL de la imagen del producto.
  * @param string $title         El título del producto.
  * @param string $description   La descripción del producto.
+ * @param array  $moreInfo      Un arreglo con información adicional sobre el producto.
  * @param string $price         El precio del producto.
  * @param string $hrefComprar   El enlace para comprar el producto.
- * @param string $hrefMasInfo   El enlace para obtener más información sobre el producto.
  * @return void
  */
-function productsCard($urlImagen, $title, $description, $price, $hrefComprar, $hrefMasInfo)
+function productsCard($cardId, $urlImagen, $title, $description, $moreInfo, $price, $hrefComprar)
 {
     echo '
         <div class="card p-2" style="height: 100%;">
@@ -20,13 +21,26 @@ function productsCard($urlImagen, $title, $description, $price, $hrefComprar, $h
                 <h5 class="mt-2 text-center">' . $title . '</h5>
                 <p class="card-text">' . $description . '</p>
                 <h3 class="text-center">$' . $price . '</h3>
-                </div>
-            <div class="text-center">
-                <a href="' . $hrefComprar . '" class="btn btn-primary rounded-pill me-2">comprar</a>
-                <a href="' . $hrefMasInfo . '" class="btn btn-primary rounded-pill">mas info</a>
             </div>
+            <div class="text-center mb-3">
+                <a href="' . $hrefComprar . '" class="btn btn-primary rounded-pill me-2">comprar</a>
+                <a id="btn-more-info-' . $cardId . '" class="btn btn-primary rounded-pill" onclick="moreProductInfo(' . $cardId . ')">
+                    Mas info
+                </a>
+            </div>
+            <div id="mas-info-' . $cardId . '" class="p-2" style="display: none;" >';
+
+    foreach ($moreInfo as $content) {
+        echo '<p class="card-text">' . $content . '</p>';
+    }
+    echo '
+            </div> 
         </div>';
+
+    //* script para mostrar u ocultar la información adicional
+    echo ' <script src="' . $GLOBALS['PUBLIC_JS'] . '/moreProductInfo.js"></script>';
 }
+
 
 
 
