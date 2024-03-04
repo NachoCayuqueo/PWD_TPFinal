bf244745d3407acc3cae65e9383f7e88aaf1dde6
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-03-04 12:48:34 -0300

    se comenzo con el abm de usuario rol

--- app/controllers/AbmUsuarioRol.php
+++ app/controllers/AbmUsuarioRol.php
@@ -1,4 +1,62 @@
 <?php
 class AbmUsuarioRol
 {
+    /**
+     * Espera como parametro un arreglo asociativo donde las claves coinciden 
+     * con los nombres de las variables instancias del objeto
+     * @param array $param
+     * @return UsuarioRol
+     */
+    private function cargarObjeto($param)
+    {
+        $obj = null;
+        if (
+            isset($param['idUsuario']) &&
+            isset($param['idRol'])
+        ) {
+            $objUsuario = new Usuario();
+            $objUsuario->setIdUsuario($param['idUsuario']);
+
+            $objetoRol = new Rol();
+            $objetoRol->setIdRol($param['idrol']);
+
+            $obj = new usuarioRol();
+            $obj->setear($objUsuario, $objetoRol);
+        }
+
+        return $obj;
+    }
+    /**
+     * 
+     * @param array $param
+     */
+    public function alta($param)
+    {
+        $resp = false;
+        $objUsuarioRol = $this->cargarObjeto($param);
+        if (($objUsuarioRol != null) && ($objUsuarioRol->insertar())) {
+            $resp = true;
+        }
+        return $resp;
+    }
+
+    /**
+     * permite buscar un objeto
+     * @param array $param
+     * @return array
+     */
+    public function buscar($param)
+    {
+        $where = [];
+        if ($param !== NULL) {
+            if (isset($param['idUsuario']))
+                $where[] = " idusuario = '" . $param['idUsuario'] . "'";
+            if (isset($param['idRol']))
+                $where[] = " idrol = '" . $param['idRol'] . "'";
+        }
+        $whereClause = implode(" AND ", $where);
+        $objetoUsuarioRol = new UsuarioRol();
+        $arreglo = $objetoUsuarioRol->listar($whereClause);
+        return $arreglo;
+    }
 }
