c56221e1734f582fb2925d1f9b0a1b5b2b90ac5c
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-03-08 12:36:20 -0300

    lista de roles

---
+++ app/views/admin/roleList.php
@@ -0,0 +1,57 @@
+<?php
+include_once '../../../config/configuration.php';
+$objetoRoles = new AbmRol();
+$listaRoles = $objetoRoles->buscar(null);
+
+$existenRoles = false;
+if (!empty($listaRoles)) {
+    $existenRoles = true;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    $title = "Roles";
+    include_once "../structures/head.php";
+    include_once "./structures/roleTable.php";
+    ?>
+</head>
+
+<body>
+    <?php
+    include_once '../structures/navbar.php';
+    ?>
+    <div class="mt-3">
+        <h1 class="text-center">Panel Administrador</h1>
+    </div>
+    <div class="container-sm p-4" style="width:70%">
+        <?php
+        if ($listaRoles) {
+            crearTablaRoles($listaRoles);
+        } else {
+            echo '
+                    <div class="container d-flex justify-content-center">
+                        <div class=" card-container d-flex justify-content-center align-items-center">
+                            <div class="card text-center mb-3" style="width: 28rem;">
+                                <div class="card-header">
+                                    <div class="text-end"><a class="btn-close" href="index.php" role="button"></a></div>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">UPS!</h5>
+                                    <p class="card-text">No se encontraron roles cargados en la base de datos</p>
+                                </div>
+                                <div class="card-footer text-body-secondary">
+                                    <img src="../../assets/logo.png" style="height: 30px;" alt="logo-fai">
+                                </div>
+                            </div>
+                        </div>
+                    </div>';
+        }
+        ?>
+    </div>
+</body>
+
+</html>
