0d5c58a2e8af0e60c12862e374f890766379c78c
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-03-20 17:43:57 -0300

    se modifico el parametro moreinfo

--- app/views/structures/cards.php
+++ app/views/structures/cards.php
@@ -7,13 +7,15 @@
  * @param string $urlImagen     La URL de la imagen del producto.
  * @param string $title         El título del producto.
  * @param string $description   La descripción del producto.
- * @param array  $moreInfo      Un arreglo con información adicional sobre el producto.
+ * @param string $moreInfo      Un string con información adicional sobre el producto separada por '.'.
  * @param string $price         El precio del producto.
  * @param string $hrefComprar   El enlace para comprar el producto.
  * @return void
  */
 function productsCard($cardId, $urlImagen, $title, $description, $moreInfo, $price, $hrefComprar)
 {
+    // Convertir el string $moreInfo en un arreglo usando explode
+    $moreInfoArray = explode('.', $moreInfo);
     echo '
         <div class="card card-container p-2" style="height: 100%;">
             <img src="' . $urlImagen . '" class="image card-img-top" alt="' . $title . '">
@@ -30,7 +32,7 @@ function productsCard($cardId, $urlImagen, $title, $description, $moreInfo, $pri
             </div>
             <div id="mas-info-' . $cardId . '" class="p-2" style="display: none;" >';
 
-    foreach ($moreInfo as $content) {
+    foreach ($moreInfoArray as $content) {
         echo '<p class="card-text">' . $content . '</p>';
     }
     echo '
@@ -40,28 +42,3 @@ function productsCard($cardId, $urlImagen, $title, $description, $moreInfo, $pri
     //* script para mostrar u ocultar la información adicional
     echo ' <script src="' . $GLOBALS['PUBLIC_JS'] . '/moreProductInfo.js"></script>';
 }
-
-
-
-
-// function productsCard($urlImagen, $title, $descriptions, $price, $hrefComprar, $hrefMasInfo)
-// {
-//     echo '
-//         <div class="card p-2" style="height: 100%;">
-//             <img src="' . $urlImagen . '" class="card-img-top" alt="' . $title . '">
-//             <div class="card-body">
-//                 <h5 class="mt-2 text-center">' . $title . '</h5>';
-
-//     foreach ($descriptions as $content) {
-//         if (is_array($content)) {
-//             echo '<p class="card-text">' . $content . '</p>';
-//         }
-//     }
-//     echo '        <h3 class="text-center">$' . $price . '</h3>
-//                 <div class="text-center">
-//                     <a href="' . $hrefComprar . '" class="btn btn-primary rounded-pill me-2">comprar</a>
-//                     <a href="' . $hrefMasInfo . '" class="btn btn-primary rounded-pill">mas info</a>
-//                 </div>
-//             </div>
-//         </div>';
-// }
