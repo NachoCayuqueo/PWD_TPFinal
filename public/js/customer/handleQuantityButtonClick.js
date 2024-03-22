0c420ee41f2d1dbe9dcda3c86ffc5dc0cc382668
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-03-22 11:40:50 -0300

    botones de compra

--- public/js/customer/handleQuantityButtonClick.js
+++ public/js/customer/handleQuantityButtonClick.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
   $("#button-minus").on("click", function () {
-    var inputValue = $("#quantity").val();
-    var newValue = parseInt(inputValue) - 1;
+    const inputValue = $("#quantity").val();
+    const newValue = parseInt(inputValue) - 1;
     if (newValue >= 0) {
       $("#quantity").val(newValue);
     }
   });
 
   $("#button-plus").on("click", function () {
-    var inputValue = $("#quantity").val();
-    var newValue = parseInt(inputValue) + 1;
+    const inputValue = $("#quantity").val();
+    const newValue = parseInt(inputValue) + 1;
     $("#quantity").val(newValue);
   });
 });
