0cd94a14980d061c0e35ab93238204f435bf371d
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-04-03 16:23:29 -0300

    comenzando a diseñar footer

--- app/views/structures/footer.php
+++ app/views/structures/footer.php
@@ -1,28 +1,62 @@
+<!-- <div class="container-fluid footer">
+    <div class="row p-5" style="background-color: #2d3f50;">
+        <div class="footer-title-with-line">
+            <img class="footer-img" src="<?php echo $BOOTSTRAP_ICONS ?>/facebook.svg" alt="facebook">
+            <img class="footer-img" src="<?php echo $BOOTSTRAP_ICONS ?>/instagram.svg" alt="instagram">
+            <img class="footer-img" src="<?php echo $BOOTSTRAP_ICONS ?>/twitter-x.svg" alt="twitter-x">
+        </div>
+        <div class="d-flex justify-content-center mt-4">
+            <img src="<?php echo $LOGOS ?>/logo_pet_shop.png" alt="inicio" width="80">
+        </div>
+        <div class="col-xs-12 mt-4">
+            <p class=" footer-title text-center">Copyright &copy; 2024 DoggyFriends</p>
+        </div>
+    </div>
+</div> -->
+
 <div class="container-fluid footer">
-    <div class="row p-5" style="background-color: #232946;">
-        <div class="col-xs-12 col-md-6 text-center">
-            <img src="<?php echo $LOGOS ?>/logo.png" alt="inicio" width="70" class="d-inline-block align-text-top">
-            <p class="footer-subtitle text-secondary mt-1">Universidad Nacional del Comahue</p>
-            <p class="footer-subtitle text-secondary">Programación Web Dinámica</p>
-            <p class="footer-subtitle text-secondary">2024</p>
+    <div class="row p-3" style="background-color: #2d3f50;">
+        <div class="text-center">
+            <img src="<?php echo $LOGOS ?>/logo_pet_shop.png" alt="inicio" width="80">
         </div>
-        <div class="col-xs-12 col-md-6">
-            <h5 class="mb-3 footer-title">Contacto</h5>
-            <div class="mb-2">
-                <img src="<?php echo $PRINCIPAL ?>/views/icons/icons/github.svg" alt="">
-                <a class=" footer-subtitle text-decoration-none" href="https://github.com/NachoCayuqueo" target="_blank">GitHub</a>
-            </div>
-            <div class="mb-2">
-                <img src="<?php echo $PRINCIPAL ?>/views/icons/icons/envelope-fill.svg" alt="">
-                <a class=" footer-subtitle text-decoration-none" href="#">nacho.cayuqueo@est.fi.uncoma.edu.ar</a>
-            </div>
-            <div class="mb-2">
-                <img src="<?php echo $PRINCIPAL ?>/views/icons/icons/envelope-fill.svg" alt="">
-                <a class=" footer-subtitle text-decoration-none" href="#">cayuqueonacho@gmail.com</a>
+        <hr class="mt-3 mb-3" style="color: white;">
+        <div class="col-xs-12 col-md-4">
+            <h5 class="mb-3 footer-title">Nosotros</h5>
+            <div class="mb-2 footer-text">
+                <p>Aute deserunt incididunt esse amet pariatur anim nisi nulla fugiat officia sit excepteur veniam.</p>
+                <a href="#" class="btn fw-bold">¿Quienes Somos?</a>
             </div>
         </div>
+        <div class="col-xs-12 col-md-4 footer-text">
+            <h5 class="mb-3 footer-title">Contactános</h5>
+            <div class="d-flex mb-2">
+                <img class="footer-img" src="<?php echo $BOOTSTRAP_ICONS ?>/whatsapp.svg" alt="whatsapp">
+                <p>numero celular</p>
+            </div>
+            <div class="d-flex mb-2">
+                <img class="footer-img" src="<?php echo $BOOTSTRAP_ICONS ?>/telephone-fill.svg" alt="telephone">
+                <p>numero telefono</p>
+            </div>
+            <div class="d-flex mb-2">
+                <img class="footer-img" src="<?php echo $BOOTSTRAP_ICONS ?>/envelope-at-fill.svg" alt="email">
+                <p>correoelectronico@gmail.com</p>
+            </div>
+            <div class="d-flex mb-2">
+                <img class="footer-img" src="<?php echo $BOOTSTRAP_ICONS ?>/geo-alt-fill.svg" alt="location">
+                <p>nuestra direccion</p>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-4">
+            <h5 class="mb-3 footer-title">Nuestras Redes sociales</h5>
+            <div class="mb-2">
+                <a href="https://www.facebook.com" target="_blank"><img class="footer-img" src="<?php echo $BOOTSTRAP_ICONS ?>/facebook.svg" alt="facebook"></a>
+                <a href="https://www.instagram.com" target="_blank"><img class="footer-img" src="<?php echo $BOOTSTRAP_ICONS ?>/instagram.svg" alt="instagram"></a>
+                <a href="https://twitter.com/?lang=es" target="_blank"><img class="footer-img" src="<?php echo $BOOTSTRAP_ICONS ?>/twitter-x.svg" alt="twitter-x"></a>
+            </div>
+        </div>
+        <hr class="mt-3 mb-3" style="color: white;">
         <div class="col-xs-12">
-            <p class=" footer-title text-center">&copy; Desarrollado por Cayuqueo, Nacho Emanuel</p>
+            <p class=" footer-title text-center">&copy; DOGGY FRIENDS Todos los derechos reservados, 2024</p>
         </div>
     </div>
 </div>
