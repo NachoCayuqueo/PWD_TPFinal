1f41a1b2f590ff3ee9c65fb71c9a4f7224af617f
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-04-09 09:30:38 -0300

    modal deshabiblitar item menu

--- app/views/admin/structures/modals.php
+++ app/views/admin/structures/modals.php
@@ -160,4 +160,71 @@ function modalEditMenu($modalId, $idRolPadre, $idItem, $nombreItem, $subMenu, $r
         ';
 }
 
+function modalDeleteMenu($modalId, $idRolPadre, $idItem, $nombreMenu, $subMenu)
+{
+    echo '
+        <div class="modal fade modal-borrar" id="' . $modalId . '" tabindex="-1" aria-labelledby="modalDeleteLabel" aria-hidden="true">
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <form id="formulario_' . $idItem . '" class="formulario-borrar-menu">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Deshabilitar Menu</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row mb-4">
+                            <div class="col form-floating">
+                                <input type="text" class="form-control" id="idMenu_' . $idItem . '" value="' . $idItem . '" name="idMenu_' . $idItem . '" readonly>
+                                <label for="idMenu_' . $idItem . '" class="ms-2">ID Menu</label>
+                            </div>
+                            <div class="col form-floating">
+                                <input type="text" class="form-control" id="nombreItem_' . $idItem . '" name="nombreItem_' . $idItem . '" value="' . $nombreMenu . '" readonly>
+                                <label for="nombreItem_' . $idItem . '" class="ms-2">Nombre</label>
+                            </div>
+                        </div>';
+    if (!empty($subMenu)) {
+        echo '<hr class="mb-3" style="color: grey;">';
+        echo '<p for="rol" class="form-label">Item del Selector</p>    
+                                                            ';
+        foreach ($subMenu as $item) {
+            $idItemSelector = $item['id'];
+            $nombreItemSelector = $item['nombre'];
+            echo '
+                                <div class="row mb-4">
+                                    <div class="col form-floating">
+                                    <input type="text" class="form-control" id="idItemSelectorBorrar_' . $idItemSelector . '_' . $idItem . '" value="' . $idItemSelector . '" name="idItemSelectorBorrar_' . $idItemSelector . '_' . $idItem . '" readonly>
+                                    <label for="idItemSelectorBorrar_' . $idItemSelector . '_' . $idItem . '" class="ms-2">ID</label>
+                                    </div>
+                                    <div class="col form-floating">
+                                        <input type="text" class="form-control" id="nombreItemSelectorBorrar_' . $idItemSelector . '_' . $idItem . '" name="nombreItemSelectorBorrar_' . $idItemSelector . '_' . $idItem . '" value="' . $nombreItemSelector . '" readonly>
+                                        <label for="nombreItemSelectorBorrar_' . $idItemSelector . '_' . $idItem . '" class="ms-2">Nombre</label>
+                                    </div>
+                                    <div class="col form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" role="switch" id="switchCheckBorrar_' . $idItemSelector . '_' . $idItem . '">
+                                        <label class="form-check-label" for="switchCheckBorrar_' . $idItemSelector . '_' . $idItem . '">Deshabilitar item</label>
+                                    </div>
+                                </div>
+                                ';
+        }
+        //* Nuevo switch
+        echo '
+                        <div class="row mb-4">
+                            <div class="col">
+                                <label for="deleteSwitch_' . $idItem . '" class="form-label">Deshabilitar el selector completo</label>
+                                <input class="form-check-input switchCheck" type="checkbox" id="deleteSwitch_' . $idItem . '">
+                            </div>
+                        </div>';
+    }
+    echo '</div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                      <button type="submit" class="btn btn-danger">Desactivar</button>
+                    </div>
+                </form>
+            </div>
+          </div>
+        </div>
+    ';
+}
+
 echo '<script src="' . $PUBLIC_JS . '/admin/buttonActionsAjax.js"></script>';
