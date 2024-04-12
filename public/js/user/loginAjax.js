236a8250a2ca4766f8a9d5c823959f58adeb4818
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-04-12 12:25:18 -0300

    encriptacion de password

---
+++ public/js/user/loginAjax.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  $("#formulario-login").submit(function (event) {
+    event.preventDefault();
+    if (validarFormularioLogin(event)) {
+      login();
+    }
+  });
+});
+
+function login() {
+  const user = $("#user").val();
+  const password = $("#password").val();
+  const encryptedPassword = hex_md5(password);
+
+  $.ajax({
+    url: "../../views/login/actions/loginAction.php",
+    type: "POST",
+    data: {
+      user,
+      password: encryptedPassword,
+    },
+    success: function (response) {
+      response = JSON.parse(response);
+      if (response.title === "EXITO") {
+        Swal.fire({
+          icon: "success",
+          title: "Éxito",
+          text: response.message,
+        }).then((result) => {
+          window.location.href = "../../../app/views/home";
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: response.message,
+        });
+      }
+    },
+    error: function (xhr, status, error) {
+      // Maneja los errores de la solicitud AJAX
+      console.error(error);
+      console.error(xhr.responseText);
+      // Muestra una alerta de error
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Hubo un error al procesar la solicitud. Por favor, inténtalo de nuevo.",
+      });
+    },
+  });
+}
