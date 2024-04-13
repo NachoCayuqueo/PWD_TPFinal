5ffbccfe276912e0546c974fdca1688ffde2d4b5
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-04-13 15:16:05 -0300

    nuevos modales agregados

--- app/views/admin/structures/modals.php
+++ app/views/admin/structures/modals.php
@@ -252,5 +252,55 @@ function modalActivarMenu($modalId, $idMenuPadre, $nombreMenu)
     ';
 }
 
+function modalActivarUsuario($idUsuario, $nombreUsuario)
+{
+    echo '
+    <div class="modal fade" id="' . $idUsuario . '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+            <form id="formulario_' . $idUsuario . '" class="formulario-activar-usuario">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Activar Usuario</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  ¿Esta seguro que desea activar al usuario ' . $nombreUsuario . '?
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                  <button type="submit" class="btn btn-primary">Activar</button>
+                </div>
+            </form>
+        </div>
+      </div>
+    </div>
+    ';
+}
+
+function modalHabilitarUsuario($idUsuario, $nombreUsuario)
+{
+    echo '
+    <div class="modal fade" id="' . $idUsuario . '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+            <form id="formulario_' . $idUsuario . '" class="formulario-habilitar-usuario">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Habilitar Usuario</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  ¿Esta seguro que desea habilitar al usuario ' . $nombreUsuario . '?
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                  <button type="submit" class="btn btn-primary">Activar</button>
+                </div>
+            </form>
+        </div>
+      </div>
+    </div>
+    ';
+}
+
 echo '<script src="' . $PUBLIC_JS . '/admin/validations.js"></script>';
 echo '<script src="' . $PUBLIC_JS . '/admin/buttonActionsAjax.js"></script>';
