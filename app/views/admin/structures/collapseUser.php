5958eedaa920beafe5c5cf498d874090296787b5
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-04-13 16:52:50 -0300

    se eliminaron funciones que no se utilizan

--- app/views/admin/structures/collapseUser.php
+++ app/views/admin/structures/collapseUser.php
@@ -5,10 +5,6 @@ function mostrarCollapse($idUsuario, $fechaDeshabilitado, $esActivo, $roles, $ro
     echo '<tr class="collapse" id="collapseUsuario' . $idUsuario . '" data-idusuario="' . $idUsuario . '">
               <td></td>
               <td colspan="3">' . crearTablaRol($idUsuario, $fechaDeshabilitado, $roles, $rolesDB) . '</td> <!-- Las tablas internas comienzan en la columna 2 -->
-          </tr>
-          <tr class="collapse" id="collapseUsuario' . $idUsuario . '" data-idusuario="' . $idUsuario . '">
-              <td></td>
-              <td colspan="3">' . crearTablaActivo($idUsuario, $fechaDeshabilitado, $esActivo) . '</td> <!-- Las tablas internas comienzan en la columna 2 -->
           </tr>';
 }
 
@@ -54,35 +50,4 @@ function crearTablaRol($idUsuario, $fechaDeshabilitado, $roles, $rolesDB)
     return $tablaRoles;
 }
 
-
-function crearTablaActivo($idUsuario, $fechaDeshabilitado, $esActivo)
-{
-    $checked = $esActivo ? 'checked' : '';
-    $mensaje = $esActivo ? "El usuario está activo" : "El usuario no está activo";
-    $disabled = $fechaDeshabilitado ? "disabled" : "";
-
-    return '
-        <table id="statusTable" class="table table-borderless table-status" >
-            <thead>
-                <tr>
-                    <th scope="col"></th>
-                    <th scope="col">Estado</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                        <td id="estadoUsuario_' . $idUsuario . '">' . $mensaje . '</td>
-                        <td>
-                          <input class="form-check-input checkBoxActiveUser" type="checkbox" value="" id="checkBoxActiveUser_' . $idUsuario . '"' . $checked . ' ' . $disabled . '>
-                        </td>
-                </tr>
-            </tbody>
-        </table>                  
-    ';
-}
-
 echo '<script src="' . $PUBLIC_JS . '/admin/updateRolesAjax.js"></script>';
-echo '<script src="' . $PUBLIC_JS . '/admin/updateUserStatusAjax.js"></script>';
-echo '<script src="' . $PUBLIC_JS . '/admin/observeCheckBoxStatus.js"></script>';
