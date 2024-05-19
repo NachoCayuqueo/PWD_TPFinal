3ce27bc6ed3403fb440e094fceb7adf462ed973b
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-05-19 17:17:51 -0300

    se modularizo y se creo una nueva funcion: actualizar rol

--- app/views/admin/actions/editRolDataAction.php
+++ app/views/admin/actions/editRolDataAction.php
@@ -4,25 +4,16 @@ include_once '../../../../config/configuration.php';
 $objetoRol = new AbmRol();
 
 $data = data_submitted();
+$response = array();
+
 $idRol = $data['idRol'];
 $nombreRol = $data['nombreRol'];
 
-$param = ["idRol" => $idRol];
-$rol   = $objetoRol->buscar($param);
+$modificacionExitosa = $objetoRol->actualizarRol($idRol, $nombreRol);
+if ($modificacionExitosa)
+    $response = array('title' => 'EXITO', 'message' => 'El Rol fue editado correctamente');
+else
+    $response = array('title' => 'ERROR', 'message' => 'Ocurrio un error al editar el rol con id: ' . $data['idRol']);
 
-$response = array();
-if (!empty($rol)) {
-    $params = [
-        "idRol" => $idRol,
-        "roDescripcion" => $nombreRol,
-    ];
-    $modificacionExitosa = $objetoRol->modificacion($params);
-    if ($modificacionExitosa)
-        $response = array('title' => 'EXITO', 'message' => 'Modificacion exitosa');
-    else
-        $response = array('title' => 'ERROR', 'message' => 'Ocurrio un error al editar el rol con id: ' . $data['idRol']);
-} else {
-    $response = array('title' => 'ERROR', 'message' => 'Rol no encontrado');
-}
 // Convertir el array a formato JSON
 echo json_encode($response);
