d31ad93e90957b2e1e1e40ff59bc72c51c2be338
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-05-31 15:20:42 -0300

    control de rol eliminado

--- app/views/admin/structures/roleTable.php
+++ app/views/admin/structures/roleTable.php
@@ -11,20 +11,25 @@ function crearTablaRoles($listaRoles)
     <tr class="text-center card-title">
         <th scope="col">ID</th>
         <th scope="col">Nombre</th>
+        <th scope="col">Deshabilitado</th>
         <th scope="col"></th>
     </tr>
     </thead>
     <tbody class="table-group-divider card-text">
     ';
     foreach ($listaRoles as $rol) {
+        $fechaEliminacion = $rol->getRoFechaEliminacion();
+        $disabled = $fechaEliminacion  ? "disabled" : "";
+
         echo "<tr>";
         echo "<td class='card-title'>" . $rol->getIdRol() . "</td>";
         echo "<td>" . $rol->getRoDescripcion() . "</td>";
+        echo "<td class='text-center'>" . dateFormat($fechaEliminacion)  . "</td>";
         echo "<td class='text-center'>
-                <a href='#' class='btn btn-outline-primary edit-btn' data-bs-toggle='modal' data-bs-target='#modalEdit_" . $rol->getIdRol() . "' type='button' data-bs-tooltip='tooltip' data-bs-placement='left' data-bs-title='Editar'>
+                <a href='#' class='btn btn-outline-primary edit-btn " . $disabled . "' data-bs-toggle='modal' data-bs-target='#modalEdit_" . $rol->getIdRol() . "' type='button' data-bs-tooltip='tooltip' data-bs-placement='left' data-bs-title='Editar' >
                     <img src='" . $GLOBALS['BOOTSTRAP_ICONS'] . "/pen.svg' alt='edit'>
                 </a>
-                <a href='#' class='btn btn-outline-danger delete-btn' data-bs-toggle='modal' data-bs-target='#modalDelete_" . $rol->getIdRol() . "' type='button' data-bs-tooltip='tooltip' data-bs-placement='right' data-bs-title='Borrar'>
+                <a href='#' class='btn btn-outline-danger delete-btn " . $disabled . "' data-bs-toggle='modal' data-bs-target='#modalDelete_" . $rol->getIdRol() . "' type='button' data-bs-tooltip='tooltip' data-bs-placement='right' data-bs-title='Borrar'>
                     <img src='" . $GLOBALS['BOOTSTRAP_ICONS'] . "/trash3.svg' alt='trash'>
                 </a>
               </td>";
