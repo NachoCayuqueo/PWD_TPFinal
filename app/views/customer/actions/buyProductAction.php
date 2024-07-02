fc26f42be17f49e475b092abe3766267338b7c8f
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-07-02 00:38:39 -0300

    session

--- app/views/customer/actions/buyProductAction.php
+++ app/views/customer/actions/buyProductAction.php
@@ -1,15 +1,23 @@
 <?php
 include_once '../../../../config/configuration.php';
 
+$session = new Session();
+$existeSesion = false;
+if ($session->validar()) {
+    $usuario = $session->getUsuario();
+    if (!is_null(($usuario))) {
+        $idUsuarioActivo = $usuario->getIdUsuario();
+    }
+}
 $data = data_submitted();
 $idProducto = $data['idProducto'];
 
 $objetoProducto = new AbmProducto();
 $datosProducto = $objetoProducto->obtenerDatosProductos($idProducto);;
 if (!is_null($datosProducto)) {
-    $response = array('title' => 'EXITO', 'message' => 'Producto encontrado.', 'productData' => $datosProducto);
+    $response = array('title' => 'EXITO', 'message' => 'Producto encontrado.', 'productData' => $datosProducto, 'idUsuario' => $idUsuarioActivo);
 } else {
-    $response = array('title' => 'ERROR', 'message' => 'No se encontro el producto.', 'productData' => []);
+    $response = array('title' => 'ERROR', 'message' => 'No se encontro el producto.', 'productData' => [], 'idUsuario' => $idUsuarioActivo);
 }
 
 echo json_encode($response);
